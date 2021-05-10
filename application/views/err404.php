d3d3c70241d12df93971b8803181263f0f7a4683
rochman414 <rochmanabdul414@gmail.com>
2021-05-11 02:42:22 +0700

    costum page 404

---
+++ application/views/err404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" class="pos-relative">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+
+    <title>Milestone Imani Prima</title>
+
+    <!-- vendor css -->
+    <link href="<?= base_url('assets/template')?>/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="<?= base_url('assets/template')?>/lib/Ionicons/css/ionicons.css" rel="stylesheet">
+
+    <!-- Bracket CSS -->
+    <link rel="stylesheet" href="<?= base_url('assets/template')?>/css/bracket.css">
+  </head>
+
+  <body class="pos-relative">
+
+    <div class="ht-100v d-flex align-items-center justify-content-center">
+      <div class="wd-lg-70p wd-xl-50p tx-center pd-x-40">
+        <h1 class="tx-100 tx-xs-140 tx-normal tx-inverse tx-roboto mg-b-0">404!</h1>
+        <h5 class="tx-xs-24 tx-normal tx-info mg-b-30 lh-5">The page your are looking for has not been found.</h5>
+        <p class="tx-16 mg-b-30">The page you are looking for might have been removed, had its name changed, or unavailable. Maybe you could try a search:</p>
+
+      </div>
+    </div><!-- ht-100v -->
+
+    <script src="<?= base_url('assets/template')?>/lib/jquery/jquery.js"></script>
+    <script src="<?= base_url('assets/template')?>/lib/popper.js/popper.js"></script>
+    <script src="<?= base_url('assets/template')?>/lib/bootstrap/bootstrap.js"></script>
+
+  </body>
+</html>
